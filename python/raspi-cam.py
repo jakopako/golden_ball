3e95fbec9a5e0d44f53294bd1096dd1901edc8d0
jakopako <jakob.dhondt@web.de>
2023-03-01 11:48:11 +0100

    testing

--- python/raspi-cam.py
+++ python/raspi-cam.py
@@ -4,7 +4,11 @@ import cv2 as cv
 import numpy as np
 
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration(main={"size": (640, 640)})
+# width = 1296
+# height = 972
+width = 640
+height = 480
+camera_config = picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (width, height)})
 
 picam2.configure(camera_config)
 picam2.start()
@@ -23,15 +27,16 @@ while True:
     frame = picam2.capture_array()
     frame = cv.flip(frame, 1)
 
-    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blurFrame = cv.medianBlur(grayFrame, 5)
-    blurFrame = cv.convertScaleAbs(blurFrame, alpha=0.7, beta=10)
+    blurFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blurFrame = cv.medianBlur(blurFrame, 5)
+    # blurFrame = cv.convertScaleAbs(blurFrame, alpha=0.7, beta=10)
     # blurFrame = cv.adaptiveThreshold(blurFrame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
     #                                  cv.THRESH_BINARY, 11, 3)
 
+    # print(blurFrame.shape)
     rows = blurFrame.shape[0] / 8
     circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.1, rows,
-                              param1=100, param2=30, minRadius=40, maxRadius=300)
+                              param1=100, param2=30, minRadius=10, maxRadius=150)
 
     # print(circles)
     # should we predict the position if hough doesn't find any circle?
@@ -39,21 +44,35 @@ while True:
         # there seems to be always at least one circle
         circles = np.round(circles).astype("int")
         chosen = None
+
         for i in circles[0, :]:
+            # check if circle is fully within the frame
+            # if  not 0 < i[0] < 640 or not 0 < i[1] < 480:
+            #     continue
+            
+            if i[0] + i[2] > width or i[0] - i[2] < 0 or i[1] + i[2] > height or i[1] - i[2] < 0:
+                #qqprint(i)
+                continue
+
+            # check distance to previous circle
+
             if chosen is None:
                 chosen = i
             if prevCircle is not None:
                 d = dist(i[0], i[1], prevCircle[0], prevCircle[1])
-                if dist(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) > d:
+                if dist(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) > d and d < 100:
                     chosen = i
-        cv.circle(frame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
-        cv.circle(frame, (chosen[0], chosen[1]),
-                  chosen[2], (255, 0, 0), 3)
+        if chosen is None:
+            chosen = prevCircle
+        if chosen is not None:
+            cv.circle(blurFrame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
+            cv.circle(blurFrame, (chosen[0], chosen[1]),
+                    chosen[2], (255, 0, 0), 3)
         prevPrevCircle = prevCircle
         prevCircle = chosen
         now = time.time()
 
-    cv.imshow("circles", frame)
+    cv.imshow("circles", blurFrame)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
